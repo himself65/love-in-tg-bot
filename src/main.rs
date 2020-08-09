128fe647418eef8e274126f75310a6408dc96ccb
himself65 <himself65@outlook.com>
2020-08-09 18:23:49 +0800

    feat: init main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use telegram_bot::*;
+use tokio::stream::StreamExt;
+
+#[tokio::main]
+async fn main() -> Result<(), telegram_bot::Error> {
+    {
+        use dotenv;
+        dotenv::dotenv().ok();
+    }
+    let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not found");
+    let api = Api::new(token);
+
+    let mut stream = api.stream();
+    while let Some(update) = stream.next().await {
+        let update = update?;
+        match update.kind {
+            UpdateKind::Message(message) => {
+                // todo
+            }
+            _ => {
+                // todo
+            }
+        }
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_load_env_file() {
+        {
+            use dotenv;
+            dotenv::dotenv().ok();
+        }
+        let token = std::env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not found");
+        assert_eq!(token, "replace_me");
+    }
 }
